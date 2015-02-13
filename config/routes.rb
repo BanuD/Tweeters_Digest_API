d8bf0ecc35c8ed24831f8b58c7cc6d05ff16e2ec
Daniel Kelley <daniel.congdon.kelley@gmail.com>
2015-02-13 15:38:34 -0800

    adds callback for twitter oauth

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users
 
   root 'welcome#index'
+  get 'auth/twitter/callback', to: 'sessions#create'
+  # can change this to be the following to make it not limited to twitter: match 'auth/:provider/callback', to: 'sessions#create'
 end
