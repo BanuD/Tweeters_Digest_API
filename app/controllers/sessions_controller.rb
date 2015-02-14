c4fdccd7126727f0d642cd8a0e0022bb6dd07625
Daniel Kelley <daniel.congdon.kelley@gmail.com>
2015-02-13 18:41:19 -0800

    modifies redirect route in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in."
+    # redirect_to "http://localhost:8888"
   end
 end
