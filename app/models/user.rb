6dd645e3b71faacaa56703382243c1a7bb9a8a0e
Daniel Kelley <daniel.congdon.kelley@gmail.com>
2015-02-13 16:22:08 -0800

    fixes user#from_omniauth to use first_or_create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,25 @@
 class User < ActiveRecord::Base
-  def self.from_omniauth(auth)
-    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
-  end
+  # def self.from_omniauth(auth)
 
-  def self.create_from_omniauth(auth)
-    create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      # check if this is returned from twitter like this
-      user.handle = auth["info"]["nickname"]
+  #   where(provider: auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  # end
+
+  # def self.create_from_omniauth(auth)
+  #   create! do |user|
+  #     user.provider = auth["provider"]
+  #     user.uid = auth["uid"]
+  #     # check if this is returned from twitter like this
+  #     user.handle = auth["info"]["nickname"]
+  #   end
+  # end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid      = auth.uid
+      user.handle     = auth.info.nickname
+      user.save
     end
   end
+
 end
