0a66b1b8b78ac0f36e803cab03b3bcc821582ec0
Aaron <aaron.karlsberg@gmail.com>
2015-02-14 19:18:59 -0800

    Confirm working associations except user class

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214054906) do
+ActiveRecord::Schema.define(version: 20150214235153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "collectors", force: true do |t|
+    t.text     "query"
+    t.string   "user_id"
+    t.string   "leader_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "leaders", force: true do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "image"
+    t.string   "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tweets", force: true do |t|
+    t.text     "content"
+    t.string   "collector_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider",                            null: false
