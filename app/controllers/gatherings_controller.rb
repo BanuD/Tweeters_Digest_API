8e46ae9de0e2f01ead2fff628ca784d6356f0f22
Aaron & Banu <pair+aaronkarlsberg.banud@devbootcamp.com>
2015-02-18 11:25:25 -0800

    update query in leader object with each creation of gathering

--- app/controllers/gatherings_controller.rb
+++ app/controllers/gatherings_controller.rb
@@ -12,7 +12,11 @@ class GatheringsController < ApplicationController
     user = User.find_by(id: params[:user_id])
     gathering = Gathering.find_by(leader_id: leader.id, user_id: user.id)
     gathering.destroy if gathering
+
     gathering = Gathering.create(leader_id: leader.id, user_id: user.id, query: params[:query], leader_handle: leader.handle) unless params[:query] == ""
+
+    leader.query = params[:query]
+    leader.save
 
     render json: {gathering: gathering}
   end
